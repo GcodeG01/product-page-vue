5a64c838605408847d2c562843772690395df06c
tabacchit <tabacchi0128@yahoo.com>
2019-10-26 20:13:27 -0400

    add my file adn all the neccesary infor for user input just need to link firebase

---
+++ src/components/pushProduct.vue
@@ -0,0 +1,33 @@
+
+<template>
+    <div>
+        
+        <p>what product are you selling: <input v-model="product" placeholder="product"></p>
+
+        <p>where type of product is it:  <input v-model="type" placeholder="type"> </p>
+        
+        <p>image refrence: <input v-model="image" placeholder="image"> </p>
+
+        <p>description of item: <input v-model="description" placeholder="description"> </p>
+
+        <p>price of item: <input v-model="price" placeholder="price"> </p>
+
+        <button v-on:click="save">save</button>   
+     </div>
+</template>
+
+
+
+<script>
+export default {
+    name: 'pushProduct'
+ 
+}
+
+</script>
+
+<style scoped>
+
+</style>
+
+                
